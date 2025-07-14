ef1bbed8742054028bef096996ed4181f253c088
yaqing <yaqings99@gmail.com>
2025-07-14 20:03:01 +0800

    revise

--- src/pic/pic_bootstrap.py
+++ src/pic/pic_bootstrap.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-# 数据
 schemes = ['Git', 'Git-crypt', 'Trivial-enc-sign', 'SGitLine', 'SGitChar']
 #schemes = ['Trivial-enc-sign', 'Git-crypt', 'SGitLine', 'SGitChar', 'Git']
 
@@ -19,17 +18,17 @@ mark = ['o', 's', '*', 'D', '^']
 
 #colour = ['brown', 'purple', 'green', 'blue', 'red']
 colour = ['brown', 'green', 'orange', 'blue', 'red']
-# 调整长宽比例为 8x6
+
 plt.figure(figsize=(6, 4))
 
-# 重新绘制图表，并为每条线使用不同的label
+
 for i, scheme in enumerate(schemes):
     plt.plot(x, costs[i], marker=mark[i], label=scheme, linewidth=1.5, color=colour[i])
 
-# 去掉标题（不设置标题）
+
 # plt.title('Cost (MB) vs Scheme')
 
-# 设置轴标签和图例
+
 plt.xlabel('# of updates', fontsize=18)
 plt.ylabel('Local storage costs (MB)', fontsize=18)
 plt.xticks(x, fontsize=16)
@@ -37,14 +36,14 @@ plt.yticks(fontsize=16)
 plt.grid(True)
 plt.legend(fontsize=16)
 
-# 添加完整的边框
+
 plt.gca().spines['top'].set_visible(True)
 plt.gca().spines['right'].set_visible(True)
 plt.gca().spines['left'].set_visible(True)
 plt.gca().spines['bottom'].set_visible(True)
 
-# 保存图表为PDF文件
+
 plt.savefig("Storage_bootstrap.pdf", format='pdf', bbox_inches='tight')
 
-# 显示图表
+
 plt.show()
